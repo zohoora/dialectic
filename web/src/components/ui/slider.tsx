eedc3a8ecc3aa881538df8b011878e7a44158757
zohoora <zohoora@gmail.com>
2025-12-03 21:52:38 -0500

    Improve slider UI with cleaner design
    - White circular thumb with cyan border and shadow
    - Cleaner, thinner track
    - Value displayed in a pill badge
    - Better hover and active states

--- web/src/components/ui/slider.tsx
+++ web/src/components/ui/slider.tsx
@@ -10,19 +10,19 @@ interface SliderProps extends Omit<React.InputHTMLAttributes<HTMLInputElement>, 
 
 const Slider = React.forwardRef<HTMLInputElement, SliderProps>(
   ({ className, label, showValue = true, value, min = 0, max = 100, ...props }, ref) => {
-    const percentage = ((Number(value) - Number(min)) / (Number(max) - Number(min))) * 100;
-
     return (
-      <div className="space-y-2">
+      <div className="space-y-3">
         {(label || showValue) && (
           <div className="flex items-center justify-between">
             {label && <span className="text-sm text-slate-400">{label}</span>}
             {showValue && (
-              <span className="text-sm font-mono text-accent-primary">{value}</span>
+              <span className="text-sm font-mono font-semibold text-slate-200 bg-void-300 px-2.5 py-1 rounded-md border border-white/10">
+                {value}
+              </span>
             )}
           </div>
         )}
-        <div className="relative">
+        <div className="relative py-1">
           <input
             type="range"
             ref={ref}
@@ -30,14 +30,31 @@ const Slider = React.forwardRef<HTMLInputElement, SliderProps>(
             min={min}
             max={max}
             className={cn(
-              "w-full h-2 rounded-full appearance-none cursor-pointer bg-void-300",
-              "[&::-webkit-slider-thumb]:appearance-none [&::-webkit-slider-thumb]:w-4 [&::-webkit-slider-thumb]:h-4 [&::-webkit-slider-thumb]:rounded-full [&::-webkit-slider-thumb]:bg-accent-primary [&::-webkit-slider-thumb]:shadow-glow-sm [&::-webkit-slider-thumb]:cursor-pointer [&::-webkit-slider-thumb]:transition-all [&::-webkit-slider-thumb]:hover:scale-110",
-              "[&::-moz-range-thumb]:w-4 [&::-moz-range-thumb]:h-4 [&::-moz-range-thumb]:rounded-full [&::-moz-range-thumb]:bg-accent-primary [&::-moz-range-thumb]:border-0 [&::-moz-range-thumb]:cursor-pointer",
+              "w-full h-1.5 rounded-full appearance-none cursor-pointer",
+              "bg-slate-700/50",
+              // Webkit (Chrome, Safari) thumb styling
+              "[&::-webkit-slider-thumb]:appearance-none",
+              "[&::-webkit-slider-thumb]:w-5 [&::-webkit-slider-thumb]:h-5",
+              "[&::-webkit-slider-thumb]:rounded-full",
+              "[&::-webkit-slider-thumb]:bg-white",
+              "[&::-webkit-slider-thumb]:border-2 [&::-webkit-slider-thumb]:border-accent-primary",
+              "[&::-webkit-slider-thumb]:shadow-lg [&::-webkit-slider-thumb]:shadow-accent-primary/30",
+              "[&::-webkit-slider-thumb]:cursor-pointer",
+              "[&::-webkit-slider-thumb]:transition-all [&::-webkit-slider-thumb]:duration-150",
+              "[&::-webkit-slider-thumb]:hover:scale-110 [&::-webkit-slider-thumb]:hover:shadow-xl",
+              "[&::-webkit-slider-thumb]:active:scale-95",
+              // Firefox thumb styling
+              "[&::-moz-range-thumb]:w-5 [&::-moz-range-thumb]:h-5",
+              "[&::-moz-range-thumb]:rounded-full",
+              "[&::-moz-range-thumb]:bg-white",
+              "[&::-moz-range-thumb]:border-2 [&::-moz-range-thumb]:border-accent-primary",
+              "[&::-moz-range-thumb]:cursor-pointer",
+              "[&::-moz-range-thumb]:transition-all",
+              // Track styling
+              "[&::-webkit-slider-runnable-track]:rounded-full",
+              "[&::-moz-range-track]:rounded-full",
               className
             )}
-            style={{
-              background: `linear-gradient(to right, rgb(34 211 238 / 0.5) 0%, rgb(34 211 238 / 0.5) ${percentage}%, rgb(26 35 46) ${percentage}%, rgb(26 35 46) 100%)`,
-            }}
             {...props}
           />
         </div>
